9d4a13651863fa741a73dadfc7d6ac3f00983e44
Orhan Gazi Kılıç <orhngazi@gmail.com>
2026-04-25 03:29:50 +0300

    shaderlar dosyadan okunabilmesi sistemi  yazılddı ve vertexten fragment'e renk gönderimi yani in/out yöntemleriyle yapmayı öğrendim.

--- 3 - Shaders/shaders/fragmentshader.glsl
+++ 3 - Shaders/shaders/fragmentshader.glsl
@@ -5,5 +5,5 @@ in vec4 vertexColor; // the input variable from the vertex shader (same name and
 
 void main() 
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = vec4(vertexColor);
 }
--- 3 - Shaders/shaders/vertexshader.glsl
+++ 3 - Shaders/shaders/vertexshader.glsl
@@ -5,6 +5,6 @@ out vec4 vertexColor; // Specify a color output to the fragment shader
 
 void main()
 {
-	gl_Position = vec4(aPos.x, 1.0); // see how we directly give a vec3 to vec4's constructor
-	vertexColor = vec4(0.5, 0.0, 0.0, 1.0) //set the output variable to a dark-red color
+	gl_Position = vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
+	vertexColor = vec4(1.0, 0.5, 0.0, 1.0); //set the output variable to a dark-red color
 }
