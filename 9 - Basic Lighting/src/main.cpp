d13d8713e03962509329238c4258584928fe1945
Orhan Gazi Kılıç <orhngazi@gmail.com>
2026-05-05 01:13:20 +0300

    Temel Işıklandırma eğitmi

--- 9 - Basic Lighting/src/main.cpp
+++ 9 - Basic Lighting/src/main.cpp
@@ -82,120 +82,48 @@ int main()
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	float vertices[] = {
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
 
-		-0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
 
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
 
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
 
-		-0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-		-0.5f,
-		-0.5f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
 
-		-0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		0.5f,
-		-0.5f,
-		0.5f,
-		-0.5f,
-	};
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f};
+	
 	// first, configure the cube's VAO (and VBO)
 	unsigned int VBO, cubeVAO;
 	glGenVertexArrays(1, &cubeVAO);
@@ -207,8 +135,12 @@ int main()
 	glBindVertexArray(cubeVAO);
 
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
+
+	// normal attribute (diffuse color ayarlayabilmek için)
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
 	unsigned int lightCubeVAO;
@@ -218,7 +150,7 @@ int main()
 	// we only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need (it's already bound, but we do it again for educational purposes)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// render loop
@@ -244,6 +176,11 @@ int main()
 		lightingShader.use();
 		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
 		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("lightPos", lightPos);
+
+		// Işık kaynağının normalleştirilmesi gerekli:
+		/* glm::vec3 norm = glm::normalize(Normal);
+		glm::vec3 lightDir = glm::normalize(lightPos); */
 
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
