dbcf47a34a16b312f1db7422efe8a965e562c5ee
Orhan Gazi Kılıç <orhngazi@gmail.com>
2026-05-12 00:27:17 +0300

    sorting yöntemi ile en uzaktakini ilk, ve bu sırayla en yakındakini de en son çizerek, blend ile transparan kısımların transparan olarak çizilip, dept test 'in yapılmasıyla ortalardaki texturun transparan kısımlarını beyaz olarak çizme sorununu düzeltmeyi öğrendim.

--- 16 - Blending/src/main.cpp
+++ 16 - Blending/src/main.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <filesystem.hpp>
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
@@ -73,10 +74,12 @@ int main()
 	// configure global opengl state
 	// -----------------------------
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// build and compile shaders
 	// -------------------------
-	Shader shader("shaders/3.1.blending.vs", "shaders/3.1.blending.fs");
+	Shader shader("shaders/3.2.blending.vs", "shaders/3.2.blending.fs");
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
@@ -180,11 +183,11 @@ int main()
 	// -------------
 	unsigned int cubeTexture = loadTexture(FileSystem::getPath("resources/textures/marble.jpg").c_str());
 	unsigned int floorTexture = loadTexture(FileSystem::getPath("resources/textures/metal.png").c_str());
-	unsigned int transparentTexture = loadTexture(FileSystem::getPath("resources/textures/grass.png").c_str());
+	unsigned int transparentTexture = loadTexture(FileSystem::getPath("resources/textures/window.png").c_str());
 
-	// transparent vegetation locations
+	// transparent window locations
 	// --------------------------------
-	vector<glm::vec3> vegetation{
+	vector<glm::vec3> windows{
 		glm::vec3(-1.5f, 0.0f, -0.48f),
 		glm::vec3(1.5f, 0.0f, 0.51f),
 		glm::vec3(0.0f, 0.0f, 0.7f),
@@ -209,6 +212,15 @@ int main()
 		// input
 		// -----
 		processInput(window);
+
+		// sort the transparent windows before rendering
+		// ---------------------------------------------
+		std::map<float, glm::vec3> sorted;
+		for (unsigned int i = 0; i < windows.size(); i++)
+		{
+			float distance = glm::length(camera.Position - windows[i]);
+			sorted[distance] = windows[i];
+		}
 
 		// render
 		// ------
@@ -239,13 +251,13 @@ int main()
 		model = glm::mat4(1.0f);
 		shader.setMat4("model", model);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
-		// vegetation
+		// windows (from furthest to nearest)
 		glBindVertexArray(transparentVAO);
 		glBindTexture(GL_TEXTURE_2D, transparentTexture);
-		for (unsigned int i = 0; i < vegetation.size(); i++)
+		for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
 		{
 			model = glm::mat4(1.0f);
-			model = glm::translate(model, vegetation[i]);
+			model = glm::translate(model, it->second);
 			shader.setMat4("model", model);
 			glDrawArrays(GL_TRIANGLES, 0, 6);
 		}
